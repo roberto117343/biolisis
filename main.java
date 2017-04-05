533d1fb0c8132f5626c015b98d8d942947a2f0f7
roberto117343 <roberto117343@gmail.com>
2017-04-05 12:48:14 +0200

    Update main.java

--- main.java
+++ main.java
@@ -1,5 +1,5 @@
 /*
- * «Copyright 2017 Roberto Reinosa» 
+ * «Copyright 2017 Roberto Reinosa»
  * 
  * This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
